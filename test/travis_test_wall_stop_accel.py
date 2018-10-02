8d23d4adfefad19997538e88f261ae096b4be688
Raspberrors Are Go <raspberror@welpal.com>
2018-10-02 14:09:23 +0000

    section 9.3 #3

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -19,6 +19,7 @@ class WallStopTest(unittest.TestCase):
 
     def test_io(self):
         left, right = self.set_and_get(400,100,100,0) #total: 600
+        time.sleep(1.0)
         self.assertTrue(left == 0 and right == 0,"can't stop")
 
         left, right = self.set_and_get(40,0,0,9) #total: 49
